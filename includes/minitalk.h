721d8021c09b198b7d041dadb692d6554e90a912
Charline Dutel le vaillant <cdutel-l@z2r5p3.42lyon.fr>
2022-04-18 16:34:05 +0200

    libft

---
+++ includes/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdutel-l <cdutel-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 16:24:19 by cdutel-l          #+#    #+#             */
+/*   Updated: 2022/04/18 16:28:05 by cdutel-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include "../libft/libft.h"
+
+#endif
