d6637a4e720baa96a949383965a5a00c5f80cd92
Charline Dutel le vaillant <cdutel-l@z2r5p3.42lyon.fr>
2022-05-12 12:26:08 +0200

    tab

--- sources/server.c
+++ sources/server.c
@@ -6,29 +6,60 @@
 /*   By: cdutel-l <cdutel-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 17:36:33 by cdutel-l          #+#    #+#             */
-/*   Updated: 2022/05/11 20:11:15 by cdutel-l         ###   ########.fr       */
+/*   Updated: 2022/05/12 12:24:22 by cdutel-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-char	ft_byte_to_dec(int resbytes)
+int	ft_iterative_power(int nb, int power)
 {
-	h
+	int	res;
+
+	res = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power != 0)
+	{
+		res *= nb;
+		power--;
+	}
+	return (res);
 }
- 
+
+char	ft_byte_to_dec(int *resbytes)
+{
+	int		i;
+	char	letter;
+	//int		total;
+	i = 0;
+	letter = 0;
+	//letter = malloc(sizeof(char) * 8 + 1);
+	while (i < 8)
+	{
+		//i = i + resbytes[i] * 2 ^ i;
+		letter = letter + resbytes[i] * ft_iterative_power(2, i);
+		i++;
+	}
+	//letter -= 9;
+	return (letter);
+}
+
 void	handler(int signum)
 {
 	char	result;
-	int		addbyte;
-	int		i;
-	int		tab[8];
+	//int		addbyte;
+	static int	i = 0;
+	static int	tab[8];
+	int			u;
 
-	addbyte = 0;
-	i = 0;
+	//addbyte = 0;
+	u = 0;
 	if (signum == SIGUSR1)
 	{
-		addbyte = addbyte * 10;
+		//addbyte = addbyte * 10;
 		tab[i] = 0;
 		i++;
 		//ft_printf("0\n");
@@ -36,12 +67,20 @@ void	handler(int signum)
 	else
 	{
 		//ft_printf("1\n");
-		addbyte = addbyte * 10 + 1;
+		//addbyte = addbyte * 10 + 1;
 		tab[i] = 1;
 		i++;
 	}
-	//result = ft_byte_to_dec(tab);
-	ft_printf("%s\n", result);
+	if (i == 8)
+	{
+		while (u < 8)
+		{
+			ft_printf("%d\n", tab[u]);
+			u++;
+		}
+	}
+	result = ft_byte_to_dec(tab);
+	ft_printf("%c\n", result);
 }
 
 int	main(void)
@@ -110,4 +149,20 @@ int	main(void)
 		addbyte = addbyte * 10 + 1;
 	result = ft_byte_to_dec(addbyte);
 	ft_printf("%s\n", result);
+} */
+
+/* char	ft_byte_to_dec(int *resbytes)
+{
+	int		i;
+	char	letter;
+	//int		total;
+	i = 1;
+	//letter = malloc(sizeof(char) * 8 + 1);
+	while (i < 8)
+	{
+		//i = i + resbytes[i] * 2 ^ i;
+		letter = letter + resbytes[i] * ft_iterative_power(2, i);
+		i++;
+	}
+	return (letter);
 } */
