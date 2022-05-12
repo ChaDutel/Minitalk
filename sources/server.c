0bf1c68ac42d1599a5e3a8e4b14eebcc1a69433e
Charline Dutel le vaillant <cdutel-l@z2r5p3.42lyon.fr>
2022-05-12 13:46:43 +0200

    print a c

--- sources/server.c
+++ sources/server.c
@@ -6,7 +6,7 @@
 /*   By: cdutel-l <cdutel-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 17:36:33 by cdutel-l          #+#    #+#             */
-/*   Updated: 2022/05/12 12:24:22 by cdutel-l         ###   ########.fr       */
+/*   Updated: 2022/05/12 13:45:34 by cdutel-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,54 +33,41 @@ char	ft_byte_to_dec(int *resbytes)
 {
 	int		i;
 	char	letter;
-	//int		total;
+
 	i = 0;
 	letter = 0;
-	//letter = malloc(sizeof(char) * 8 + 1);
 	while (i < 8)
 	{
-		//i = i + resbytes[i] * 2 ^ i;
 		letter = letter + resbytes[i] * ft_iterative_power(2, i);
 		i++;
 	}
-	//letter -= 9;
 	return (letter);
 }
 
 void	handler(int signum)
 {
-	char	result;
-	//int		addbyte;
+	char		result;
 	static int	i = 0;
 	static int	tab[8];
 	int			u;
 
-	//addbyte = 0;
 	u = 0;
 	if (signum == SIGUSR1)
 	{
-		//addbyte = addbyte * 10;
 		tab[i] = 0;
 		i++;
-		//ft_printf("0\n");
 	}
 	else
 	{
-		//ft_printf("1\n");
-		//addbyte = addbyte * 10 + 1;
 		tab[i] = 1;
 		i++;
 	}
 	if (i == 8)
 	{
-		while (u < 8)
-		{
-			ft_printf("%d\n", tab[u]);
-			u++;
-		}
+		i = 0;
+		result = ft_byte_to_dec(tab);
+		ft_printf("%c\n", result);
 	}
-	result = ft_byte_to_dec(tab);
-	ft_printf("%c\n", result);
 }
 
 int	main(void)
@@ -165,4 +152,60 @@ int	main(void)
 		i++;
 	}
 	return (letter);
+} */
+
+
+/* char	ft_byte_to_dec(int *resbytes)
+{
+	int		i;
+	char	letter;
+	//int		total;
+	i = 0;
+	letter = 0;
+	//letter = malloc(sizeof(char) * 8 + 1);
+	while (i < 8)
+	{
+		//i = i + resbytes[i] * 2 ^ i;
+		letter = letter + resbytes[i] * ft_iterative_power(2, i);
+		i++;
+	}
+	//letter -= 9;
+	return (letter);
+}
+
+void	handler(int signum)
+{
+	char	result;
+	//int		addbyte;
+	static int	i = 0;
+	static int	tab[8];
+	int			u;
+
+	//addbyte = 0;
+	u = 0;
+	if (signum == SIGUSR1)
+	{
+		//addbyte = addbyte * 10;
+		tab[i] = 0;
+		i++;
+		//ft_printf("0\n");
+	}
+	else
+	{
+		//ft_printf("1\n");
+		//addbyte = addbyte * 10 + 1;
+		tab[i] = 1;
+		i++;
+	}
+	if (i == 8)
+	{
+		while (u < 8)
+		{
+			ft_printf("%d\n", tab[u]);
+			u++;
+		}
+		i = 0;
+		result = ft_byte_to_dec(tab);
+		ft_printf("%c\n", result);
+	}
 } */
